7f6fc108516890b6ecd273a4c8b7abd8d5d5ffb9
James <jameshaig12@gmail.com>
2022-10-26 18:49:24 +0100

    Created poison ivy sword

---
+++ Items/Weapons/PoisonIvy.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using SwordsPlus.Items.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace SwordsPlus.Items.Weapons
+{
+    public class PoisonIvy : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Poison Ivy"); 
+            Tooltip.SetDefault("Dont Touch That!");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 85;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = 3;
+            Item.knockBack = 7;
+            Item.value = 2500;
+            Item.rare = 2;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.Poisoned, 180);
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.JungleGrass, 0f, 0f, 0, default(Color), 1f);
+            Main.dust[dust].noGravity = false;
+            Main.dust[dust].velocity *= 0f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddRecipeGroup("Wood", 20);
+            recipe.AddIngredient(ItemID.SilverBar, 20);
+            recipe.AddIngredient(ItemID.Stinger, 10);
+            recipe.AddIngredient(ItemID.Vine, 15);
+            recipe.AddIngredient(ItemID.SoulofNight, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
