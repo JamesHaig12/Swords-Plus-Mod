7023f7428338d0aabe2229945d569afbc858a6f8
James <jameshaig12@gmail.com>
2022-10-31 18:34:55 +0000

    Added Stormblade and new proj

---
+++ Items/Projectiles/WhiteLightningProjectile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+
+
+namespace SwordsPlus.Items.Projectiles
+{
+    public class WhiteLightningProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lightning Projectile"); // This will never be seen but is needed(?)
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee; // Can be modified by melee accessories/armour
+            Projectile.damage = 55; // For this proj we will set a damage
+            Projectile.width = 12; // Hitbox
+            Projectile.height = 12; // Hitbox
+            Projectile.aiStyle = 0; // Bullet AI Style, will travel straight
+            Projectile.friendly = true; // No player damage
+            Projectile.hostile = false; // No player damage
+            Projectile.penetrate = 2; // Will penetrate 2 enemies before dispersing
+            Projectile.timeLeft = 600; // 600 Frames aka 10 seconds of screentime
+            Projectile.light = 2f; // Light emission
+            Projectile.ignoreWater = false; // Will slow in water
+            Projectile.tileCollide = true; // On collision disperses
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            SoundEngine.PlaySound(new SoundStyle("SwordsPlus/Sounds/FlameImpact").WithVolumeScale(10f).WithPitchOffset(.3f));
+        }
+    }
+}
---
+++ Items/Weapons/WhiteStormBlade.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using SwordsPlus.Items.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.DataStructures;
+
+namespace SwordsPlus.Items.Weapons
+{
+    public class WhiteStormBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("White Stormblade"); // By default, capitalization in classnames will add spaces to the display name.
+            Tooltip.SetDefault("Strike Your Enemies With The Wrath Of Zues\nCredits To AgentB90");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 30;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 80;
+            Item.height = 80;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = 1;
+            Item.knockBack = 6;
+            Item.value = 2500;
+            Item.rare = 3;
+            Item.crit = 4;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.StarWrath; // change this to lightning projectile ModContent.ProjectileType<WhiteLightningProjectile>();
+            Item.shootSpeed = 8f;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddRecipeGroup("Wood", 20);
+            recipe.AddIngredient(ItemID.SilverBar, 20);
+            recipe.AddIngredient(ItemID.Feather, 5);
+            recipe.AddIngredient<Items.SwordSoul>(10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+
+            Recipe recipe2 = CreateRecipe();
+            recipe2.AddRecipeGroup("Wood", 20);
+            recipe2.AddIngredient(ItemID.GoldBar, 20);
+            recipe2.AddIngredient(ItemID.Feather, 5);
+            recipe2.AddIngredient<Items.SwordSoul>(10);
+            recipe2.AddTile(TileID.Anvils);
+            recipe2.Register();
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockBack)
+        {
+            Vector2 target = Main.screenPosition + new Vector2((float)Main.mouseX, (float)Main.mouseY);
+            float ceilingLimit = target.Y;
+            if (ceilingLimit > player.Center.Y - 200f)
+            {
+                ceilingLimit = player.Center.Y - 200f;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                position = player.Center + new Vector2((-(float)Main.rand.Next(0, 401) * player.direction), -600f);
+                position.Y -= (100 * i);
+                Vector2 heading = target - position;
+                if (heading.Y < 0f)
+                {
+                    heading.Y *= -1f;
+                }
+                if (heading.Y < 20f)
+                {
+                    heading.Y = 20f;
+                }
+                heading.Normalize();
+                heading *= new Vector2(velocity.X, velocity.Y).Length();
+                velocity.X = heading.X;
+                velocity.Y = heading.Y + Main.rand.Next(-40, 41) * 0.02f;
+                Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage * 2, knockBack, player.whoAmI, 0, ceilingLimit);
+            }
+            return false;
+        }
+    }     
+}
