a27313401bac3f58bf6569d2593333eefb095103
James Haig <jameshaig12@gmail.com>
2022-11-11 12:34:52 +0000

    update

--- Items/Projectiles/CardProjectile.cs
+++ Items/Projectiles/CardProjectile.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
 
 namespace SwordsPlus.Items.Projectiles
 {
     public class CardProjectile : ModProjectile
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Playing Card Projectile"); // This will never be seen but is needed(?)
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.width = 12; // Hitbox
+            Projectile.height = 20; // Hitbox
+            Projectile.aiStyle = 0; // Bullet AI Style, will travel straight
+            Projectile.friendly = true; // No player damage
+            Projectile.hostile = false; // No player damage
+            Projectile.penetrate = 0; // Will penetrate 2 enemies before dispersing
+            Projectile.timeLeft = 600; // 600 Frames aka 10 seconds of screentime
+            Projectile.light = 0f; // Light emission
+            Projectile.ignoreWater = false; // Will slow in water
+            Projectile.tileCollide = false; // On collision disperses
+        }
+
+        public override void AI()
+        {
+            // Creating dusts (particle effects) for the projectile, mix of 3 materials at different sizes
+
+            int dust = Dust.NewDust(Projectile.Center, 12, 12, DustID.Flare, 0f, 0f, 0, default(Color), (float)Main.rand.Next(1, 5));
+            Main.dust[dust].noGravity = true;
+            Main.dust[dust].velocity *= 3f;
+
+            int dust2 = Dust.NewDust(Projectile.Center, 12, 12, DustID.Torch, 0f, 0f, 0, default(Color), (float)Main.rand.Next(1, 5));
+            Main.dust[dust2].noGravity = true;
+            Main.dust[dust2].velocity *= 3f;
+
+            int dust3 = Dust.NewDust(Projectile.Center, 12, 12, DustID.MinecartSpark, 0f, 0f, 0, default(Color), 3);
+            Main.dust[dust3].noGravity = true;
+            Main.dust[dust3].velocity *= 3f;
+
+            int dust4 = Dust.NewDust(Projectile.Center, 12, 12, DustID.MinecartSpark, 0f, 0f, 0, default(Color), 2);
+            Main.dust[dust4].noGravity = true;
+            Main.dust[dust4].velocity *= 3f;
+
+
+        }
     }
 }
--- Items/Weapons/QueenOfHearts.cs
+++ Items/Weapons/QueenOfHearts.cs
@@ -14,7 +14,7 @@ namespace SwordsPlus.Items.Weapons
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("The Queen Of Hearts"); // By default, capitalization in classnames will add spaces to the display name.
-            Tooltip.SetDefault("");
+            Tooltip.SetDefault("'I warn you... If i lose my temper, You lose your head!'");
         }
 
         public override void SetDefaults()
@@ -23,17 +23,40 @@ namespace SwordsPlus.Items.Weapons
             Item.DamageType = DamageClass.Melee;
             Item.width = 80;
             Item.height = 80;
-            Item.useTime = 20;
-            Item.useAnimation = 20;
+            Item.useTime = 35;
+            Item.useAnimation = 35;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.knockBack = 6;
+            Item.knockBack = 6f;
             Item.value = 2500;
             Item.rare = ItemRarityID.Orange;
             Item.crit = 4;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.shoot = ModContent.ProjectileType<CardProjectile>();
-            Item.shootSpeed = 3f;
+            Item.shootSpeed = 9f;
         }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.LifeCrystal, 5);
+            recipe.AddIngredient(ItemID.TitaniumBar, 20);
+            recipe.AddIngredient(ItemID.Obsidian, 20);
+            recipe.AddIngredient<Items.AdvSwordSoul>(10);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+
+        /*public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockBack)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Vector2 pertubedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(12));
+                Projectile.NewProjectile(source, position.X, position.Y, pertubedSpeed.X, pertubedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return true;
+        }*/
+        
+        
     }
 }
