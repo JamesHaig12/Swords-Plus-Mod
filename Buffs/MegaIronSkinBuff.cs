840a5f77c4a049b72884297bd9cdc2c0dfb932d8
JamesHaig12 <jameshaig12@gmail.com>
2022-11-18 00:00:47 +0000

    mega iron skin change

--- Buffs/MegaIronSkinBuff.cs
+++ Buffs/MegaIronSkinBuff.cs
@@ -9,14 +9,14 @@ namespace SwordsPlus.Buffs
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Mega Ironskin");
-            Description.SetDefault("Grants +12 defense.");
+            Description.SetDefault("Grants +16 defense.");
             Main.buffNoTimeDisplay[Type] = false;
             Main.debuff[Type] = false;
         }
 
         public override void Update(Player player, ref int buffIndex)
         {
-            player.statDefense += 12; // Grant a +12 defense boost to the player while the buff is active
+            player.statDefense += 16; // Grant a +12 defense boost to the player while the buff is active
         }
     }
 }
